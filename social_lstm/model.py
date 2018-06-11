5c55e6af35753e0c10fb8d80cfb137e912af04f8
Zac Chan <sunshinecltzac@gmail.com>
2018-06-11 22:39:27 +0800

    bug fix

--- social_lstm/model.py
+++ social_lstm/model.py
@@ -76,7 +76,7 @@ class SocialLSTMModel:
             self.increment = tf.constant(1.0, name="increment")
         # Containers to store output distribution parameters
         with tf.name_scope("Distribution_parameters_stuff"):
-            self.initial_output = tf.split(0, args.maxNumPeds, tf.zeros([args.maxNumPeds, self.output_size]))
+            self.initial_output = tf.split(tf.zeros([args.max_num_peds, self.output_size]), args.max_num_peds, axis=0)
         # Tensor to represent non-existent ped
         with tf.name_scope("Non_existent_ped_stuff"):
             nonexistent_ped = tf.constant(0.0, name="zero_ped")
@@ -104,7 +104,7 @@ class SocialLSTMModel:
 
                 with tf.name_scope("concatenate_embeddings"):
                     # Concatenate the embeddings
-                    complete_input = tf.concat(1, [embedded_spatial_input, embedded_tensor_input])
+                    complete_input = tf.concat([embedded_spatial_input, embedded_tensor_input], axis=1)
 
                 with tf.variable_scope("LSTM") as scope:
                     if seq > 0 or ped > 0:
@@ -119,8 +119,8 @@ class SocialLSTMModel:
                     target_ped_id = frame_target_data[seq][ped, 0]
 
                 with tf.name_scope("get_coef"):
-                    z = self.initial_output
-                    o_mux, o_muy, o_sx, o_sy, o_corr = tf.split(1, 5, z)
+                    z = self.initial_output[ped]
+                    o_mux, o_muy, o_sx, o_sy, o_corr = tf.split(z, 5, axis=1)
                     # The output must be exponentiated for the std devs
                     o_sx = tf.exp(o_sx)
                     o_sy = tf.exp(o_sy)
@@ -143,10 +143,10 @@ class SocialLSTMModel:
                 with tf.name_scope("increment_cost"):
                     self.cost = tf.cond(
                         tf.logical_or(tf.equal(ped_id, nonexistent_ped), tf.equal(target_ped_id, nonexistent_ped)),
-                        self.cost, tf.add(self.cost, lossfunc))
+                        lambda: self.cost, lambda: tf.add(self.cost, lossfunc))
                     self.counter = tf.cond(
                         tf.logical_or(tf.equal(ped_id, nonexistent_ped), tf.equal(target_ped_id, nonexistent_ped)),
-                        self.counter, tf.add(self.counter, self.increment))
+                        lambda: self.counter, lambda: tf.add(self.counter, self.increment))
         #############################################################################
 
         with tf.name_scope("mean_cost"):
@@ -183,7 +183,7 @@ class SocialLSTMModel:
         # Calculate sx*sy
         sxsy = tf.multiply(sx, sy)
         # Calculate the exponential factor
-        z = tf.square(tf.div(normx, sx)) + tf.square(tf.div(normy, sy)) - 2*tf.div(tf.mul(rho, tf.mul(normx, normy)), sxsy)
+        z = tf.square(tf.div(normx, sx)) + tf.square(tf.div(normy, sy)) - 2*tf.div(tf.multiply(rho, tf.multiply(normx, normy)), sxsy)
         negRho = 1 - tf.square(rho)
         # Numerator
         result = tf.exp(tf.div(-z, 2*negRho))
@@ -201,26 +201,26 @@ class SocialLSTMModel:
         output_states : A list of tensors each of shape 1 x RNN_size of length MNP
         '''
         # Create a zero tensor of shape MNP x (GS**2) x RNN_size
-        social_tensor = tf.zeros([self.args.maxNumPeds, self.grid_size*self.grid_size, self.lstm_num], name="social_tensor")
+        social_tensor = tf.zeros([self.args.max_num_peds, self.grid_size*self.grid_size, self.lstm_num], name="social_tensor")
         # Create a list of zero tensors each of shape 1 x (GS**2) x RNN_size of length MNP
-        social_tensor = tf.split(social_tensor, self.args.maxNumPeds, axis=0)
+        social_tensor = tf.split(social_tensor, self.args.max_num_peds, axis=0)
         # Concatenate list of hidden states to form a tensor of shape MNP x RNN_size
         hidden_states = tf.concat(self.output_states, axis=0)
         # Split the grid_frame_data into grid_data for each pedestrians
         # Consists of a list of tensors each of shape 1 x MNP x (GS**2) of length MNP
-        grid_frame_ped_data = tf.split(grid_frame_data, self.args.maxNumPeds, axis=0)
+        grid_frame_ped_data = tf.split(grid_frame_data, self.args.max_num_peds, axis=0)
         # Squeeze tensors to form MNP x (GS**2) matrices
         grid_frame_ped_data = [tf.squeeze(input_, [0]) for input_ in grid_frame_ped_data]
 
         # For each pedestrian
-        for ped in range(self.args.maxNumPeds):
+        for ped in range(self.args.max_num_peds):
             # Compute social tensor for the current pedestrian
             with tf.name_scope("tensor_calculation"):
                 social_tensor_ped = tf.matmul(tf.transpose(grid_frame_ped_data[ped]), hidden_states)
-                social_tensor[ped] = tf.reshape(social_tensor_ped, [1, self.grid_size*self.grid_size, self.rnn_size])
+                social_tensor[ped] = tf.reshape(social_tensor_ped, [1, self.grid_size*self.grid_size, self.lstm_num])
 
         # Concatenate the social tensor from a list to a tensor of shape MNP x (GS**2) x RNN_size
-        social_tensor = tf.concat(0, social_tensor)
+        social_tensor = tf.concat(social_tensor, axis=0)
         # Reshape the tensor to match the dimensions MNP x (GS**2 * RNN_size)
-        social_tensor = tf.reshape(social_tensor, [self.args.maxNumPeds, self.grid_size*self.grid_size*self.rnn_size])
+        social_tensor = tf.reshape(social_tensor, [self.args.max_num_peds, self.grid_size*self.grid_size*self.lstm_num])
         return social_tensor
